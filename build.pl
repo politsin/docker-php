04b166754ba31a76ae5d3efe2d8ebe5b7b99dc2e
Anatoly <politsin@gmail.com>
2017-06-20 13:46:36 +0300

    Update build.pl

--- build.pl
+++ build.pl
@@ -2,6 +2,6 @@
 
 use strict;
 use warnings;
-chdir("/opt/image-docker-php");
+chdir("/opt/docker-php");
 system("docker build -t docker-php:1.5 .");
 system("docker tag docker-php:1.5 docker-php:latest");
