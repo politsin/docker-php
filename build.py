3ce4829b0040dd6afb01de9a40d2235ca20ce5a7
Anatoly Politsin <politsin@gmail.com>
2017-10-21 16:37:36 +0300

    syntax error

--- build.py
+++ build.py
@@ -9,13 +9,13 @@ repo = "docker-php"
 if os.path.exists('/opt/docker-php'):
     os.chdir('/opt/docker-php')
     os.system("docker build -f Dockerfile -t %s:%s ." % (repo, ver))
-    os.system("docker tag %s:%s %s:latest" % (repo, ver, repo)))
+    os.system("docker tag %s:%s %s:latest" % (repo, ver, repo))
 
 # get travis repo
 if os.environ['REPO']:
     repo = os.environ['REPO']
     bild = os.environ['TRAVIS_BUILD_NUMBER']
     os.system("docker build -f Dockerfile -t %s ." % (repo))
-    os.system("docker tag %s %s:latest" % (repo, repo)))
-    os.system("docker tag %s %s:%s" % (repo, repo, ver)))
-    os.system("docker tag %s %s:%s.%s" % (repo, repo, ver, bild)))
+    os.system("docker tag %s %s:latest" % (repo, repo))
+    os.system("docker tag %s %s:%s" % (repo, repo, ver))
+    os.system("docker tag %s %s:%s.%s" % (repo, repo, ver, bild))
