375723c4467f53d85b29c95acff863c1659119d8
Anatoly Politsin <politsin@gmail.com>
2023-09-01 16:23:38 +0300

    fix version

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # cd /opt/build/docker-php
 docker build . -t synstd/php
 docker tag synstd/php synstd/php:8.1
-docker tag synstd/php synstd/php:8.1.302
+docker tag synstd/php synstd/php:8.1.301
 docker push synstd/php
 docker push synstd/php:8.1
-docker push synstd/php:8.1.302
+docker push synstd/php:8.1.301
